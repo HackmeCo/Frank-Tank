f1690a8fcc1b7db4063afb5e2ef251b10512252f
KathrynmHansen <katie112892@aol.com>
2016-09-07 11:38:54 -0500

    adds user and id to the database

---
+++ client/components/UsersPage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import $ from '../models/lib/jquery';
+
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state
+}
